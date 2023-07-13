0b6c2a1a232658fa5ed7b1fdde7f208360d22579
GustavoAnjos99 <132578544+GustavoAnjos99@users.noreply.github.com>
2023-07-13 14:12:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,111 @@
 let turnos;
 let nomesTimes = [];
 let pontosTimes = [];
+let combinacoesTimes = [];
 
 function exibirTable() {
-    var div = document.getElementById("idtable");
-    div.style.display = "block";
+  var div = document.getElementById("idtable");
+  div.style.display = "block";
 }
+
 
 function iniciarTorneio() {
   turnos = parseInt(document.getElementById("turnosSelect").value);
-  
-  nomesTimes = [
-    document.getElementById("nomeTime1Input").value,
-    document.getElementById("nomeTime2Input").value,
-    document.getElementById("nomeTime3Input").value
-  ];
-  
-  pontosTimes = [0, 0, 0];
-  
+  const quantidadeTimes = parseInt(document.getElementById("quantidadeTimesSelect").value);
+
+  nomesTimes = [];
+  for (let i = 1; i <= quantidadeTimes; i++) {
+    nomesTimes.push(document.getElementById(`nomeTime${i}Input`).value);
+  }
+
+  pontosTimes = Array(quantidadeTimes).fill(0);
+
   atualizarTabelaPontuacao();
   gerarRodadas();
 }
 
+function atualizarTorneio() {
+  const quantidadeTimes = parseInt(document.getElementById("quantidadeTimesSelect").value);
+  var divTime4 = document.getElementById("nomeTime4Input2");
+
+  if (quantidadeTimesSelect.value === "4") {
+    divTime4.style.display = "block";
+  } else {
+    divTime4.style.display = "none";
+    nomeTime4Input.value = "";
+  }
+
+  if (quantidadeTimes === 3) {
+    document.getElementById("time4InputContainer").style.display = "none";
+    document.getElementById("time4Row").style.display = "none";
+  } else if (quantidadeTimes === 4) {
+    document.getElementById("time4InputContainer").style.display = "block";
+    document.getElementById("time4Row").style.display = "table-row";
+  }
+}
+
 function atualizarTabelaPontuacao() {
-  document.getElementById("nomeTime1").innerText = nomesTimes[0];
-  document.getElementById("nomeTime2").innerText = nomesTimes[1];
-  document.getElementById("nomeTime3").innerText = nomesTimes[2];
-  
-  document.getElementById("pontosTime1").innerText = pontosTimes[0];
-  document.getElementById("pontosTime2").innerText = pontosTimes[1];
-  document.getElementById("pontosTime3").innerText = pontosTimes[2];
+  for (let i = 0; i < nomesTimes.length; i++) {
+    document.getElementById(`nomeTime${i + 1}`).innerText = nomesTimes[i];
+    document.getElementById(`pontosTime${i + 1}`).innerText = pontosTimes[i];
+  }
 }
 
 function gerarRodadas() {
-  const partidasPorTurno = 3;
-  
-  let rodadasHtml = "";
-  
-  const combinacoesTimes = [
-    [0, 1],
-    [1, 2],
-    [2, 0]
-  ];
-  
-  for (let turno = 1; turno <= turnos; turno++) {
-    rodadasHtml += `<h3>Turno ${turno}</h3>`;
-    
-    for (let partida = 1; partida <= partidasPorTurno; partida++) {
-      const combinacao = combinacoesTimes[(partida - 1 + turno - 1) % 3];
-      
-      const time1 = nomesTimes[combinacao[0]];
-      const time2 = nomesTimes[combinacao[1]];
-      
-      rodadasHtml += `<p>${time1} x ${time2}: <br> <input type="number" id="placarTime_${turno}_${partida}_1" min="0"> X <input type="number" id="placarTime_${turno}_${partida}_2" min="0"> <button onclick="atualizarPontuacao(${turno}, ${partida})">Atualizar</button></p>`;
-    }
+  if (nomesTimes.length === 3) {
+    combinacoesTimes = [
+      [0, 1],
+      [1, 2],
+      [2, 0]
+    ];
+  } else if (nomesTimes.length === 4) {
+    combinacoesTimes = [
+      [0, 1],
+      [2, 3],
+      [1, 2],
+      [3, 0],
+      [1, 3],
+      [0, 2]
+    ];
   }
-  
+
+  const partidasPorTurno = combinacoesTimes.length;
+  const totalPartidas = turnos * partidasPorTurno;
+
+  let rodadasHtml = "";
+
+  for (let partida = 1; partida <= totalPartidas; partida++) {
+    const turno = Math.ceil(partida / partidasPorTurno);
+    const combinacao = combinacoesTimes[(partida - 1) % combinacoesTimes.length];
+
+    const time1 = nomesTimes[combinacao[0]];
+    const time2 = nomesTimes[combinacao[1]];
+
+    if (partida % partidasPorTurno === 1) {
+      rodadasHtml += `<h3>Turno ${turno}</h3>`;
+    }
+
+    rodadasHtml += `<p>${time1} x ${time2}: <br> <input type="number" id="placarTime_${turno}_${partida}_1" min="0"> X <input type="number" id="placarTime_${turno}_${partida}_2" min="0"> <button onclick="atualizarPontuacao(${turno}, ${partida})">Atualizar</button></p>`;
+  }
+
   document.getElementById("rodadasContainer").innerHTML = rodadasHtml;
 }
 
 function atualizarPontuacao(turno, partida) {
   const placarTime1 = parseInt(document.getElementById(`placarTime_${turno}_${partida}_1`).value);
   const placarTime2 = parseInt(document.getElementById(`placarTime_${turno}_${partida}_2`).value);
-  
+
+  const indice = (partida - 1) % combinacoesTimes.length;
+  const resultado = combinacoesTimes[indice];
+
   if (placarTime1 > placarTime2) {
-    pontosTimes[(partida - 1 + turno - 1) % 3] += 3;
-  } else if (placarTime1 === placarTime2) {
-    pontosTimes[(partida - 1 + turno - 1) % 3] += 1;
-    pontosTimes[(partida - 1 + turno) % 3] += 1;
-  } else {
-    pontosTimes[(partida - 1 + turno) % 3] += 3;
-  }
+    pontosTimes[resultado[0]] += 3;
+   } else if (placarTime1 === placarTime2) {
+      pontosTimes[resultado[0]] += 1;
+      pontosTimes[resultado[1]] += 1;
+    } else {
+      pontosTimes[resultado[1]] += 3;
+    }
   
-  atualizarTabelaPontuacao();
-}
+    atualizarTabelaPontuacao();
+  }
